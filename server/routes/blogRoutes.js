4376860c57d74b6a9e6329fa2c3af2500d1b7ee8
coderman982 <khanfuzail955@gmail.com>
2026-04-15 15:11:01 +0530

    jk

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, deleteBlogById, getAllBlogs, getBlogById, tooglePublish } from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
 import auth from "../middleware/auth.js";
 
 const blogRouter=express.Router();
 //whenver add api  it will go to addblog and do all the things
 blogRouter.post("/add",  upload.single('image'),auth,addBlog);//auth for authenticaton
+blogRouter.get('/all',getAllBlogs);
+blogRouter.get('/:blogId',getBlogById);
+blogRouter.post('/delete',deleteBlogById);
+blogRouter.post('/toggle-publish',auth,tooglePublish);
+
 
 //upload.single('image') we will send image from front end it will parse it
 //and send to req.file then medaikit will upload the file
